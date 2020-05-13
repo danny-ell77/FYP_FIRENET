22a236fd9f682369c316d2e0358b2ccfd79d265b
Olah Daniel <danielolahskybrow77@gmail.com>
2020-05-13 13:09:54 -0700

    added preprocess.py to convert images to trfrecord format

--- preprocess.py
+++ preprocess.py
@@ -1,27 +1,56 @@
-import os
-import csv
+import tensorflow as tf
+import numpy as np
+from Train import make_input_fn
 
-path = 'C:\Users\user\Desktop\PROJECT\Dataset\fire-dataset-kaggle\fire_dataset\fire_images'
 
-with open('output.csv', 'w',
-          newline='') as csvfile:  # Loop through path and add all files matching *.jpg to array files
-    files = []
-    for r, d, f in os.walk(path):
-        for _file in f:
-            if '.jpg' in _file:
-                files.append(_file)
+GCS_PATTERN = 'gs://fire_dataset/*/*.jpg'
+GCS_OUTPUT = 'gs://fire_dataset/tfrecords-jpeg-192x192-2/fire'  # prefix for output file names
+SHARDS = 16
+TARGET_SIZE = [192, 192]
+CLASSES = [b'fire', b'normal']  # do not change, maps to the labels in the data (folder names)
 
-    writer = csv.writer(csvfile, delimiter=',')  # Create a writer from csv module
-    for f in files:  # find type of file
-        t = f[0:-5]  # cut off the number and .jpg from file, leaving only the type (this may have to be changed.)
 
-        if "dog" in t:
-            t = 0
-        if "cat" in t:
-            t = 1
-        if "mouse" in t:
-            t = 2
-        if "elephant" in t:
-            t = 3
 
-        writer.writerow([f, t])  # write the row to the file output.csv
+def _bytestring_feature(list_of_bytestrings):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=list_of_bytestrings))
+
+def _int_feature(list_of_ints):  # int64
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=list_of_ints))
+
+
+def _float_feature(list_of_floats):  # float32
+    return tf.train.Feature(float_list=tf.train.FloatList(value=list_of_floats))
+
+
+def to_tfrecord(tfrec_filewriter, img_bytes, label, height, width):
+    class_num = np.argmax(np.array(CLASSES) == label)  # 'roses' => 2 (order defined in CLASSES)
+    one_hot_class = np.eye(len(CLASSES))[class_num]  # [0, 0, 1, 0, 0] for class #2, roses
+
+    feature = {
+        "image": _bytestring_feature([img_bytes]),  # one image in the list
+        "class": _int_feature([class_num]),  # one class in the list
+
+        # additional (not very useful) fields to demonstrate TFRecord writing/reading of different types of data
+        "label": _bytestring_feature([label]),  # fixed length (1) list of strings, the text label
+        "size": _int_feature([height, width]),  # fixed length (2) list of ints
+        "one_hot_class": _float_feature(one_hot_class.tolist())  # variable length  list of floats, n=len(CLASSES)
+    }
+    return tf.train.Example(features=tf.train.Features(feature=feature))
+
+
+print("Writing TFRecords")
+for shard, (image, label, height, width) in enumerate(dataset):
+    # batch size used as shard size here
+    shard_size = image.numpy().shape[0]
+# good practice to have the number of records in the filename
+    filename = GCS_OUTPUT + "{:02d}-{}.tfrec".format(shard, shard_size)
+
+    with tf.io.TFRecordWriter(filename) as out_file:
+        for i in range(shard_size):
+            example = to_tfrecord(out_file,
+                                  image.numpy()[i],  # re-compressed image: already a byte string
+                                  label.numpy()[i],
+                                  height.numpy()[i],
+                                  width.numpy()[i])
+            out_file.write(example.SerializeToString())
+        print("Wrote file {} containing {} records".format(filename, shard_size))
