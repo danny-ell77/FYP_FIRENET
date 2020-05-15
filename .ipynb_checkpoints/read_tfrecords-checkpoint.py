f68b9277f81d4db1c3b72ba238624d3b46475539
danielolah <danielolahskybrow77@gmail.com>
2020-05-15 20:57:46 +0000

    modified read_tfrecords.py

--- .ipynb_checkpoints/read_tfrecords-checkpoint.py
+++ .ipynb_checkpoints/read_tfrecords-checkpoint.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import numpy as np
-AUTO = tf.data.experimantal.AUTOTUNE
-GCS_OUTPUT = 'gs://fire_dataset/tfrecords-jpeg-192x192-2/fire'  # prefix for output file names
+AUTO = tf.data.experimental.AUTOTUNE
+GCS_OUTPUT = 'gs://qwiklabs-gcp-02-c7f1ded04a7e/fire_dataset/tfrecords-jpeg-192x192-2'  # prefix for output file names
 
-TARGET_SIZE = [192, 192]
+TARGET_SIZE = [256, 256]
 
 
 def read_tfrecord(example):
@@ -23,7 +23,7 @@ def read_tfrecord(example):
     # VarLenFeature fields require additional sparse_to_dense decoding
 
     image = tf.image.decode_jpeg(example['image'], channels=3)
-    image = tf.reshape(image, [TARGET_SIZE, 3])
+    image = tf.reshape(image, [*TARGET_SIZE, 3])
 
     class_num = example['class']
 
@@ -45,4 +45,5 @@ filenames = tf.io.gfile.glob(GCS_OUTPUT + "*.tfrec")
 dataset4 = tf.data.TFRecordDataset(filenames, num_parallel_reads=AUTO)
 dataset4 = dataset4.with_options(option_no_order)
 dataset4 = dataset4.map(read_tfrecord, num_parallel_calls=AUTO)
-dataset4 = dataset4.shuffle(300)
+for image, class_num, label, height, width, one_hot_class in dataset4.take(10):
+    print("Image shape {}, {}x{} px, class={} ({:>10}, {})".format(image.numpy().shape, width, height, class_num,label.numpy().decode('utf8'),one_hot_class))
